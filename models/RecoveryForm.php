42f8c82007f1729f907d541ac933037187384d7d
cjtterabyte <cjtterabytellc@gmail.com>
2018-06-03 18:41:34 -0400

    Fix Yii 2.1.

--- models/RecoveryForm.php
+++ models/RecoveryForm.php
@@ -109,7 +109,7 @@ class RecoveryForm extends Model
         if ($user instanceof User) {
             /** @var Token $token */
             $token = \Yii::createObject([
-                'class' => Token::className(),
+                '__class' => Token::class,
                 'user_id' => $user->id,
                 'type' => Token::TYPE_RECOVERY,
             ]);
