5a08786d73ea56c7a185a222674cdc740899625a
cjtterabyte <cjtterabytellc@gmail.com>
2018-06-04 13:36:20 -0400

    Fix View BS4 and Helpers.

--- src/user/views/admin/index.php
+++ src/user/views/admin/index.php
@@ -35,7 +35,8 @@ $this->params['breadcrumbs'][] = $this->title;
     'layout'       => "{items}\n{pager}",
     'columns' => [
         [
-            'attribute' => Yii::t('user', 'id'),
+        	'label' => Yii::t('user', 'Id'),
+            'attribute' => 'id',
             'headerOptions' => ['style' => 'width:90px;'], # 90px is sufficient for 5-digit user ids
         ],
         [
@@ -51,7 +52,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'attribute' => 'registration_ip',
             'value' => function ($model) {
                 return $model->registration_ip == null
-                    ? '<span class="not-set">' . Yii::t('user', '(not set)') . '</span>'
+                    ?  Html::tag('span', Yii::t('user', '(not set)'), ['class' => 'not-set']) 
                     : $model->registration_ip;
             },
             'format' => 'html',
@@ -61,39 +62,38 @@ $this->params['breadcrumbs'][] = $this->title;
             'attribute' => 'created_at',
             'value' => function ($model) {
                 if (extension_loaded('intl')) {
-                    return Yii::t('user', '{0, date, MMMM dd, YYYY HH:mm}', [$model->created_at]);
-                } else {
+                    return Yii::t('user', '{0, date, dd/MM/YYYY HH:mm}', [$model->created_at]);
+                    } else {
                     return date('Y-m-d G:i:s', $model->created_at);
                 }
             },
         ],
-
         [
-          'label' => Yii::t('user', 'Last Login'),      
-          'attribute' => 'last_login_at',
-          'value' => function ($model) {
-            if (!$model->last_login_at || $model->last_login_at == 0) {
-                return Yii::t('user', 'Never');
-            } else if (extension_loaded('intl')) {
-                return Yii::t('user', '{0, date, MMMM dd, YYYY HH:mm}', [$model->last_login_at]);
-            } else {
-                return date('Y-m-d G:i:s', $model->last_login_at);
-            }
-          },
+            'label' => Yii::t('user', 'Last Login'),      
+            'attribute' => 'last_login_at',
+            'value' => function ($model) {
+                if (!$model->last_login_at || $model->last_login_at == 0) {
+                    return Yii::t('user', 'Never');
+                    } else if (extension_loaded('intl')) {
+                        return Yii::t('user', '{0, date, dd/MM/YYYY HH:mm}', [$model->last_login_at]);
+                        } else {
+                            return date('Y-m-d G:i:s', $model->last_login_at);
+                }
+            },
         ],
         [
             'header' => Yii::t('user', 'Confirmation'),
             'value' => function ($model) {
                 if ($model->isConfirmed) {
-                    return '<div class="text-center">
-                                <span class="text-success">' . Yii::t('user', 'Confirmed') . '</span>
-                            </div>';
-                } else {
-                    return Html::a(Yii::t('user', 'Confirm'), ['confirm', 'id' => $model->id], [
-                        'class' => 'btn btn-xs btn-success btn-block',
-                        'data-method' => 'post',
-                        'data-confirm' => Yii::t('user', 'Are you sure you want to confirm this user?'),
-                    ]);
+                    return  Html::beginTag('div', ['class' => 'text-center']) .
+                        Html::tag('span', Yii::t('user', 'Confirmed'), ['class' => 'text-success']) .
+                    Html::endTag('div');                    
+                    } else {
+                        return Html::a(Yii::t('user', 'Confirm'), ['confirm', 'id' => $model->id], [
+                            'class' => 'btn btn-sm btn-success btn-block',
+                            'data-method' => 'post',
+                            'data-confirm' => Yii::t('user', 'Are you sure you want to confirm this user?'),
+                        ]);
                 }
             },
             'format' => 'raw',
@@ -101,57 +101,63 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
         [
             'header' => Yii::t('user', 'Block <br/>Status'),
+        	'headerOptions' => ['style' => 'text-align:center;'],
             'value' => function ($model) {
                 if ($model->isBlocked) {
                     return Html::a(Yii::t('user', 'Unblock'), ['block', 'id' => $model->id], [
-                        'class' => 'btn btn-xs btn-success btn-block',
+                        'class' => 'btn btn-sm btn-success btn-block',
                         'data-method' => 'post',
                         'data-confirm' => Yii::t('user', 'Are you sure you want to unblock this user?'),
                     ]);
-                } else {
-                    return Html::a(Yii::t('user', 'Block'), ['block', 'id' => $model->id], [
-                        'class' => 'btn btn-xs btn-danger btn-block',
-                        'data-method' => 'post',
-                        'data-confirm' => Yii::t('user', 'Are you sure you want to block this user?'),
-                    ]);
+                    } else {
+                        return Html::a(Yii::t('user', 'Block'), ['block', 'id' => $model->id], [
+                            'class' => 'btn btn-sm btn-danger btn-block',
+                            'data-method' => 'post',
+                            'data-confirm' => Yii::t('user', 'Are you sure you want to block this user?'),
+                        ]);
                 }
             },
             'format' => 'raw',
         ],
         [
-            'header' => Yii::t('user', 'Actions');
+            'header' => Yii::t('user', 'Actions'),
             '__class' => yii\grid\ActionColumn::class,
             'template' => '{switch} {resend_password} {update} {delete}',
             'buttons' => [
                 'switch' => function ($url, $model) {
                     if(\Yii::$app->user->identity->isAdmin && $model->id != Yii::$app->user->id && Yii::$app->getModule('user')->enableImpersonateUser) {
-                        return Html::a('<span class="fas fa-user"></span>', ['/user/admin/switch', 'id' => $model->id], [
-                            'title' => Yii::t('user', 'Become this user'),
-                            'data-confirm' => Yii::t('user', 'Are you sure you want to switch to this user for the rest of this Session?'),
-                            'data-method' => 'POST',
-                        ]);
+                        return Html::beginTag('a', [
+                            'data-method'=> 'POST', 
+                            'data-confirm' => Yii::t('user', 'Are you sure you want to switch to this user for the rest of this Session?'), 
+                            'title' => Yii::t('user', 'Become this user'), 
+                            'href' => ' '. Url::to(['/user/admin/switch', 'id' => $model->id]) . ' ']) .
+                            Html::tag('span', '', ['class' => 'fas fa-user']) .                        
+                        Html::endTag('a');
                     }
                 },
             	'resend_password' => function ($url, $model, $key) {
                 	if (\Yii::$app->user->identity->isAdmin && !$model->isAdmin) {
-                    	return '<a data-method="POST" data-confirm="' . Yii::t('user', 'Are you sure?') . '" href="' . Url::to(['resend-password', 'id' => $model->id]) . '">
-                			<span title="' . Yii::t('user', 'Generate and send new password to user') . '" class="fas fa-envelope">
-                			</span></a>';
+                        return Html::beginTag('a', [
+                            'data-method'=> 'POST', 
+                            'data-confirm' => ' ' . Yii::t('user', 'Are you sure?') . ' ',
+                            'href' => ' ' . Url::to(['resend-password', 'id' => $model->id]) . ' ']) .
+                            Html::tag('span', '', [
+                                'title' => ' '. Yii::t('user', 'Generate and send new password to user') . ' ', 
+                                'class' => 'fas fa-envelope']) .
+                        Html::endTag('a');
                 	}	
             	}, 
             	'update' => function ($url, $model) {
-                	return Html::a('<span class="fas fa-edit"></span>', $url, [
-                    	'title' => Yii::t('user', 'Update');
-                	]);
+                	return Html::a(Html::tag('span', '', ['class' => 'fas fa-edit']), 
+                        $url, ['title' => Yii::t('user', 'Update')]
+                    );
             	},
             	'delete' => function ($url, $model) {
-                	return Html::a('<span class="fas fa-trash"></span>', $url, [
-                    	'title' => Yii::t('user', 'Delete');
-                	]);
-            	},             
-            ],                      
+                	return Html::a(Html::tag('span', '', ['class' => 'fas fa-trash']), 
+                        $url, ['title' => Yii::t('user', 'Delete')]
+                    );
+            	}            
+            ]                      
         ],
     ],
 ]); ?>
-
-
